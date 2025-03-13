fa66bcffcee295865f69f40472cbb1174d93bba2
unknown <motunrayonak>
2025-03-13 16:52:15 +0100

    integer and float

---
+++ 003_numeric_data.py
@@ -0,0 +1,27 @@
+num_1=5
+num_2=10.5
+print(num_1+num_2)
+print(num_1-num_2)
+print(num_1*num_2)
+print(num_1/num_2)
+print(num_1//num_2)
+print(num_1**num_2)
+print(num_1%num_2)
+print((10+3)*(2**2)/(4-1))
+print(abs(-15))
+print(round(num_2,1))
+print(round(num_2,2))
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 > num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+num_2=int(num_2)
+print(num_2)
+num_1=float(num_1)
+print(num_1)
+num_3="20"
+num_3=int(num_3)
+print(num_3)
+
