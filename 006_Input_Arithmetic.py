2738807b2d74ad12e02135e800dd8993468bbfbe
motunrayonak <motunrayonak@gmail.com>
2025-03-18 18:54:25 +0100

    input

---
+++ 006_Input_Arithmetic.py
@@ -0,0 +1,16 @@
+x=int(input("enter 1st number"))
+y=float(input("enter 2nd number"))
+
+z=x+y
+print(z)
+
+z=x-y
+print(z)
+
+z=x*y
+print(z)
+
+z=x/y
+print(z)
+
+
---
+++ 007_extract_extension.py
@@ -0,0 +1,10 @@
+x=input("enter a filename:")
+
+y=x.split(".")[-1]
+
+print(y)
+
+
+
+
+
