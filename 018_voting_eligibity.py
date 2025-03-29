107e796dbfcb076c5e7634031fa230d5c6ef5532
motunrayonak <motunrayonak@gmail.com>
2025-03-29 14:16:04 +0100

    voting and age clarification created

---
+++ 018_voting_eligibity.py
@@ -0,0 +1,14 @@
+age=int(input("enter age:"))
+nationality=input("enter nationality:")
+
+if age >= 18 and nationality =="nigeria":
+     print("you are eligible to vote in nigeria")
+
+if age >18 and nationality == "nigeria":
+        print("you are too young to vote")
+
+if nationality != "nigeria":
+    print("you must be a nigerian citizen to vote here")
+
+
+
---
+++ 019_age_clarification.py
@@ -0,0 +1,14 @@
+age=int(input("enter age"))
+
+if age <13:
+    print("you are a child")
+
+elif 13 <= age <= 19:
+    print("you are a teenager")
+
+elif 20 <= age <= 59:
+    print("you are an adult")
+
+else:
+    print("you are a senior cetizen")
+
