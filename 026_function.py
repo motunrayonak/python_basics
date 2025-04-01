ae22f9f6d220d1b0e2b531bb8251591357442d66
motunrayonak <motunrayonak@gmail.com>
2025-04-01 10:43:39 +0100

    function created

---
+++ 026_function.py
@@ -0,0 +1,19 @@
+def greet_user ():
+    print("Hello,")
+    print("welcome to python programming!")
+greet_user()
+
+def add_numbers (a,b):
+    c=a+b
+    print(c)
+add_numbers(7,6)
+
+def multiply_numbers (a,b):
+    c=a*b
+    return c
+result=multiply_numbers(6,9)
+print(result)
+
+
+
+
